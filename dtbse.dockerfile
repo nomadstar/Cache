3887696bf91b809b85384fc940bdcf9c536de86b
N0m4d <ignacio.marambio_z@mail.udp.cl>
2024-04-21 01:26:54 -0400

    Explicación

--- dtbse.dockerfile
+++ dtbse.dockerfile
@@ -11,8 +11,8 @@ COPY steam_reviews.csv .
 # Ejecuta comando de la capeta de origen, NO BORRES EL ARCHIVO NI ESTE COMANDO
 COPY docker-entrypoint.sh /docker-entrypoint-initdb.d/
 
-# Increase the maximum number of connections
+# Aumenta las conexiones para que se coma todas las filas
 RUN echo "max_connections = 1000" >> /usr/share/postgresql/postgresql.conf.sample
 
-# Expose the PostgreSQL port
+# expone el puerto
 EXPOSE 5432
--- lazybash.sh
+++ lazybash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 set -e
+#borra todo en caso de que haya algo, ideal para borrar errores por redundancia de imagenes
 trap 'echo "Ha ocurrido un error en la línea $LINENO. Error de construcción de Docker. Revise los logs para más detalles."' ERR
 if [ "$(docker image ls -q tusuperimagen)" ]; then
     docker image rm tusuperimagen
@@ -8,8 +9,10 @@ fi
 if [ "$(docker image ls -q tusuperclient)" ]; then
     docker image rm tusuperclient
 fi
+#crea imagenes necesarias para el docker compose
 docker build -t tusuperimagen -f dtbse.dockerfile .
 docker build -t tusuperclient -f client.dockerfile .
+#se explica solo
 docker compose up -d
 
 
